5021156190e295f9e69939eb7392947b33209954
siddu <siddalinga1729@gmail.com>
2025-01-19 13:08:58 +0530

    resolved bild issues

--- app/page.tsx
+++ app/page.tsx
@@ -221,7 +221,7 @@ export default function Home() {
                   <path fillRule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd" />
                 </svg>
                 <span className="text-gray-400">Languages:</span>
-                <span className="text-gray-300">English, Kannada, Hindi</span>
+                <span className="text-gray-300">English,Telugu, Kannada, Hindi</span>
               </div>
             </div>
           </header>
